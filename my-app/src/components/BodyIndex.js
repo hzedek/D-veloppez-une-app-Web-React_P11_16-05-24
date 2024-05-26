55b4682c0c2a2e73ebf8d3e5762c265727c3f27d
hzed <hzedek59@gmail.com>
2024-05-26 22:16:56 +0200

    card function done

--- my-app/src/components/BodyIndex.js
+++ my-app/src/components/BodyIndex.js
@@ -1,15 +1,25 @@
 import DisplayBanner from '../assets/displayBanner.jpg'
 import '../styles/BodyIndex.scss'
+import data from "../logements.json"
 
 
 function Display() {
-    return <img src={DisplayBanner} alt="bannière"></img> 
+    const Titre = "Chez vous, Partout et ailleurs"
+    return <div><img src={DisplayBanner} alt="bannière"/><p>{Titre}</p></div>
+}
+function CardLocation() {
+    return (
+        <ul>
+        {data.map((card) => (
+            <li key={card.id}><img src={card.cover} alt="logement"/> <p>{card.title}</p></li>
+        ))}
+    </ul>
+    )
 }
 
 
-
 function BodyIndex() {
-    return <div><Display /></div>
+    return <div className='body-banner'><Display /><CardLocation /></div>
 }
 
 export default BodyIndex
